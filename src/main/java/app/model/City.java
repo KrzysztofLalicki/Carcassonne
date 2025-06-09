f896fb51a7f521cec26c7ea5c4434bca382dd06c
Krzysztof Lalicki <krzysztof.lalicki@student.uj.edu.pl>
2025-06-09 09:24:54 +0200

    City points fixed

--- src/main/java/app/model/City.java
+++ src/main/java/app/model/City.java
@@ -19,8 +19,11 @@ public class City extends Area {
     public void mergeWith(Area area) {
         super.mergeWith(area);
         if (area instanceof City c) {
-            pennants += c.pennants;
-            holes += c.holes - 2;
+            if (c != this) {
+                pennants += c.pennants;
+                holes += c.holes;
+            }
+            holes -= 2;
         }
     }
 
